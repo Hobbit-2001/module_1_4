0d8a42f3396ad2ff92c55fda1631d80d57550259
Alexandr <70943203+Hobbit-2001@users.noreply.github.com>
2024-11-07 00:04:08 +0300

    Initial commit

---
+++ module_1_4.py
@@ -0,0 +1,5 @@
+homework = 12
+hours = 1.5
+a = 1.5 / 12
+сourse_name = 'Python'
+print ("Курс:", сourse_name + ",", "всего задач:", str(homework)  + ",", "затрачено часов:",str(hours) + ",", "среднее время выполнения", a)
